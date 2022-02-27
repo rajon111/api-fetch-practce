ee5635153cc51e03e380625372e1b2ffca36fe41
rajon <mostofarajon111@gmail.com>
2022-02-28 00:13:46 +0600

    country deails shows when you click button-added

--- js/rest-countries.js
+++ js/rest-countries.js
@@ -6,18 +6,37 @@ const loadCountries = () =>{
 loadCountries()
 
 const showCountries =countries =>{
-    // console.log(countries)
+     // console.log(countries)
     const countryDiv = document.getElementById('countries')
     for(const country of countries){
-        console.log(country)
+        // console.log(country)
         const div = document.createElement('div')
         div.classList.add('country')
         div.innerHTML=`
+        <img width=150px src="${country.flag}" alt="">
         <h3> ${country.name} </h3>
         <p> ${country.capital} </p> 
-        <img width=150px src="${country.flag}" alt="">
-        <p> ${country.currencies[0].code} </p>
+        
+        
+        <button onclick="loadCountryByname('${country.name}')"> details </button>
         `;
         countryDiv.appendChild(div)
     }
+}
+const loadCountryByname =name=>{
+    const url =`https://restcountries.com/v2/name/${name}`
+    fetch(url)
+    .then(res=> res.json())
+    .then(data => displayCountryDetail(data[0]))
+}
+
+const displayCountryDetail = country =>{
+    const countryDetailsDiv = document.getElementById('country-detils')
+    countryDetailsDiv.innerHTML=`
+    <h3> ${country.name} </h3>
+        <p> ${country.capital} </p> 
+        <p> ${country.population} </p> 
+        <img width=150px src="${country.flag}" alt="">
+    `;
+    
 }
